45b542a6dc7d8ecba2638ebcd4081fc3366e5f4f
anastasiiayakymiv <137912980+anastasiiayakymiv@users.noreply.github.com>
2023-12-09 14:04:27 +0200

    Add files via upload

--- scripts/swiper.js
+++ scripts/swiper.js
@@ -1,12 +1,18 @@
-      var swiper = new Swiper(".mySwiper", {
-        cssMode: true,
-        navigation: {
-          nextEl: ".swiper-button-next",
-          prevEl: ".swiper-button-prev",
-        },
-        pagination: {
-          el: ".swiper-pagination",
-        },
-        mousewheel: true,
-        keyboard: true,
-      });
+var swiper2 = new Swiper("#galleryswiper", {
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+var swiper = new Swiper("#mySwiper", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+        delay: 5000,
+        disableOnInteraction: false,
+    },
+    navigation: {
+      nextEl: ".button-rooms"
+    },
+});
