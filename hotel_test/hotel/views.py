d2c3b637ef486582e570414969f6a4d363627ba2
yfedeiko <yfedeiko@hotels.com>
2024-04-17 15:45:39 +0300

    added post

--- hotel_test/hotel/views.py
+++ hotel_test/hotel/views.py
@@ -1,8 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 
 def index(request):
+    if request.method == "POST":
+        a = request.POST.get("startDate", "")
+        b = request.POST.get("finishDate", "")
+        print(a, b)
+        return redirect("/booking",)
     return render(request, "hotel/index.html")
 
 
